8db307beae022f08e8b669f1af109ec5f48b9696
TomDonoghue <tdonoghue@ucsd.edu>
2018-07-01 01:50:10 -0700

    Add examples with single PSD

---
+++ mat_py_mat/c_1PSD_matlab_analysis.m
@@ -0,0 +1,9 @@
+%% Load and Analyze FOOOF Results in Matlab
+
+% Load FOOOF results
+fooof_results = load('fooof_results');
+
+% Check out FOOOF results
+fooof_results
+
+%% Now you can do anything you want with your FOOOF results
