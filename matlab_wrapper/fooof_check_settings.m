18ed4577df9bb40476b836a82eb342782b8e8226
TomDonoghue <tdonoghue@ucsd.edu>
2018-07-12 17:51:26 -0700

    Fix error in updating settings

--- matlab_wrapper/fooof_check_settings.m
+++ matlab_wrapper/fooof_check_settings.m
@@ -3,23 +3,28 @@
 
 function settings = fooof_check_settings(settings)
 
-    if ~exist('settings.peak_width_limits')
-        settings.peak_width_limits = [0.5, 12];
-    end
-    if ~exist('settings.max_n_peaks')
-        settings.max_n_peaks = Inf;
-    end
-    if ~exist('settings.min_peak_amplitude')
-        settings.min_peak_amplitude = 0.0;
-    end
-    if ~exist('settings.peak_threshold')
-        settings.peak_threshold = 2.0;
-    end
-    if ~exist('settings.background_mode')
-        settings.background_mode = 'fixed';
-    end
-    if ~exist('settings.verbose')
-        settings.verbose = true;
-    end
+     if ~isfield(settings, 'peak_width_limits')
+         settings.peak_width_limits = [0.5, 12];
+     end
+
+     if ~isfield(settings, 'max_n_peaks')
+         settings.max_n_peaks = Inf;
+     end
+
+     if ~isfield(settings, 'min_peak_amplitude')
+         settings.min_peak_amplitude = 0.0;
+     end
+
+     if ~isfield(settings, 'peak_threshold')
+         settings.peak_threshold = 2.0;
+     end
+
+     if ~isfield(settings, 'background_mode')
+         settings.background_mode = 'fixed';
+     end
+
+     if ~isfield(settings, 'verbose')
+         settings.verbose = true;
+     end
 
 end
