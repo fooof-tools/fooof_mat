a533ac46e628bb7f5aa9cb2fa388f198d4f611d6
TomDonoghue <tdonoghue@ucsd.edu>
2018-09-25 11:08:53 -0700

    Revert some changes

--- matlab_wrapper/fooof.m
+++ matlab_wrapper/fooof.m
@@ -49,8 +49,8 @@ function fooof_results = fooof(freqs, psd, f_range, settings)
     % Run FOOOF fit
     fm.fit(freqs, psd, f_range)
 
-    
     % Extract outputs
-    fooof_results = fooof_unpack_results(fm);
+    fooof_results = fm.get_results();
+    fooof_results = fooof_unpack_results(fooof_results);
     
 end
--- matlab_wrapper/fooof_unpack_results.m
+++ matlab_wrapper/fooof_unpack_results.m
@@ -1,18 +1,6 @@
 % Unpack fooof_results python object into matlab struct
-function results_out = fooof_unpack_results(fm)
+function results_out = fooof_unpack_results(results_in)
 
-    results_out = struct();
-    
-    results_out.fooof_freqs = double(py.array.array('d',fm.freqs));
-    results_out.power_spectrum = double(py.array.array('d', fm.power_spectrum));
-    results_out.fooof_spectrum = double(py.array.array('d', fm.fooofed_spectrum_));
-    
-    %This still gives error in matlab due to the variable starting with undefined character
-    % results_out.fooof_bg_fit = double(py.array.array('d',fm._bg_fit));
-    % results_out.fooof_bg_fit = double(py.array.array('d',getfield(fm,'_bg_fit')));
-     
-    results_in = fm.get_results();
-    
     results_out.background_params = ...
         double(py.array.array('d', results_in.background_params));
     
@@ -27,7 +15,9 @@ function results_out = fooof_unpack_results(fm)
     results_out.error = ...
         double(py.array.array('d', py.numpy.nditer(results_in.error)));
     
-    % Note: for reasons unknown, r_squared seems to come out as float...
+    % Note: r_squared seems to come out as float
+    %   It's not clear why this value is different, but doesn't seem
+    %   to require the type casting like other (code commented below)
     results_out.r_squared = results_in.r_squared;
     %results_out.r_squared = ...
     %    double(py.array.array('d', py.numpy.nditer(results_in.r_squared)));
