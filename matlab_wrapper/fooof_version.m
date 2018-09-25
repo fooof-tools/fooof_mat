60fa6a6d46a5aec4d19a12b44dc89245614a2ea0
TomDonoghue <tdonoghue@ucsd.edu>
2018-09-25 12:57:53 -0700

    add version file

---
+++ matlab_wrapper/fooof_version.m
@@ -0,0 +1,14 @@
+function fooof_version()
+
+    % Get the version of the Python implementation of FOOOf being used
+    fooof_py_version = string(py.pkg_resources.get_distribution('fooof').version);
+
+    % Set the version number of the matlab wrapper
+    %   Note: this is where version is defined for this wrapper 
+    fooof_wrapper_version = "0.1.0";
+
+    % Display versions
+    disp('Current version of FOOOF:     ' + fooof_py_version)
+    disp('Current version of Wrapper:   ' + fooof_wrapper_version)
+    
+end
