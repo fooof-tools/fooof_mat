c4489dd7aeccfa441dd673469562889427195bb7
TomDonoghue <tdonoghue@ucsd.edu>
2018-09-25 13:24:33 -0700

    Rewrite check_settings, including checking for nans

--- matlab_wrapper/fooof_check_settings.m
+++ matlab_wrapper/fooof_check_settings.m
@@ -3,28 +3,20 @@
 
 function settings = fooof_check_settings(settings)
 
-     if ~isfield(settings, 'peak_width_limits')
-         settings.peak_width_limits = [0.5, 12];
-     end
-
-     if ~isfield(settings, 'max_n_peaks')
-         settings.max_n_peaks = Inf;
-     end
-
-     if ~isfield(settings, 'min_peak_amplitude')
-         settings.min_peak_amplitude = 0.0;
-     end
-
-     if ~isfield(settings, 'peak_threshold')
-         settings.peak_threshold = 2.0;
-     end
-
-     if ~isfield(settings, 'background_mode')
-         settings.background_mode = 'fixed';
-     end
-
-     if ~isfield(settings, 'verbose')
-         settings.verbose = true;
-     end
+    % Set defaults for all settings
+    defaults = struct(...
+        'peak_width_limits', [0.5, 12], ...
+        'max_n_peaks', Inf, ...
+        'min_peak_amplitude', 0.0, ...
+        'peak_threshold', 2.0, ...
+        'background_mode', 'fixed', ...
+        'verbose', true);
+    
+    % Overwrite any non-existent of nan settings with defaults
+    for field = fieldnames(defaults)'
+        if ~isfield(settings, field) || isnan(settings.(field{1}))
+            settings.(field{1}) = defaults.(field{1});
+        end
+    end
 
 end
